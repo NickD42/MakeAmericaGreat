eac32277338ce6bd1b7725caf11af91d8f159cc2
spartem <nidoucette@hotmail.com>
2016-11-19 01:18:37 -0400

    level select updated

---
+++ Make America Great Again/Assets/Scripts/SelectStageScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectStageScript : MonoBehaviour {
+
+    Ray ray;
+    RaycastHit hit;
+
+    Color tmp;
+
+    // Use this for initialization
+    void Start ()
+    {
+        tmp = this.GetComponent<SpriteRenderer>().color;
+        tmp.a = 0f;
+        this.GetComponent<SpriteRenderer>().color = tmp;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            Color tmp = this.GetComponent<SpriteRenderer>().color;
+            tmp.a = 100f;
+            this.GetComponent<SpriteRenderer>().color = tmp;
+        }
+        else
+        {
+
+        }
+    }
+}
